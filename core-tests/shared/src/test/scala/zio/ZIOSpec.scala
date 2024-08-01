206ce019d77e4b32d3cf3fc7e30a6b41dabd4d1d
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-08-01 12:35:05 +0530

    add tests

--- core-tests/shared/src/test/scala/zio/ZIOSpec.scala
+++ core-tests/shared/src/test/scala/zio/ZIOSpec.scala
@@ -213,22 +213,22 @@ object ZIOSpec extends ZIOBaseSpec {
           assert(c)(equalTo(d)) &&
           assert(d)(not(equalTo(e)))
       },
-      test("handles interruptions correctly") {
-        def effect(ref: Ref[Boolean]): ZIO[Any, Nothing, Unit] =
-          for {
-            _ <- ref.set(true)
-            _ <- ZIO.never // Simulates a long-running effect
-          } yield ()
-
+      test("cachedInvalidate blocks cancelation") {
         for {
-          ref                  <- Ref.make(false)
-          (cached, invalidate) <- effect(ref).cachedInvalidate(60.minutes)
-          fiber                <- cached.fork
-          _                    <- ZIO.sleep(1.second)
-          _                    <- fiber.interrupt
-          _                    <- invalidate
-          result <- cached.timeout(1.second) // Ensure the effect is not hanging
-        } yield assert(result)(isNone)       // Should be None as the effect should be interrupted and invalidated
+          startWaiting <- Promise.make[Nothing, Unit]
+          ref          <- Ref.make(true)
+          (call, invalidate) <- ZIO
+                                  .ifZIO(ref.get)(
+                                    onTrue = ref.set(false) *> ref.get,
+                                    onFalse = startWaiting.succeed(()) *> ZIO.never *> ref.get
+                                  )
+                                  .cachedInvalidate(Duration.Infinity)
+          first     <- call
+          _         <- invalidate
+          callFiber <- call.fork
+          _         <- startWaiting.await
+          _ <- callFiber.interrupt // Here - test never ends
+        } yield assert(first)(Assertion.equalTo(false))
       }
     ),
     suite("catchNonFatalOrDie")(

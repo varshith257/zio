596f88202dd2e787d784e5c4fe08db7b24f03419
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-08-01 14:14:54 +0530

    debug

--- core-tests/shared/src/test/scala/zio/ZIOSpec.scala
+++ core-tests/shared/src/test/scala/zio/ZIOSpec.scala
@@ -238,6 +238,7 @@ object ZIOSpec extends ZIOBaseSpec {
           _      <- startWaiting.await
           _      <- ZIO.debug("Awaited startWaiting promise")
           _      <- ZIO.debug("Interrupting callFiber") *> callFiber.interrupt
+          _      <- TestClock.adjust(1.second)
           second <- callFiber.join
           _      <- ZIO.debug(s"Second call result: $second")
         } yield assert(first)(equalTo(false)) && assert(second)(equalTo(false))

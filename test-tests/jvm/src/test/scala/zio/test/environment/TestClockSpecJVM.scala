9010002688e5367ff0f784f4a8d08b67c4205e6a
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-08-06 11:29:23 +0530

    mv tests

--- test-tests/jvm/src/test/scala/zio/test/environment/TestClockSpecJVM.scala
+++ test-tests/jvm/src/test/scala/zio/test/environment/TestClockSpecJVM.scala
@@ -99,24 +99,24 @@ object TestClockSpecJVM extends ZIOBaseSpec {
             scheduler               <- ZIO.blocking(Clock.scheduler)
             scheduledExecutorService = scheduler.asScheduledExecutorService
             future <- ZIO.logInfo("Scheduling task...") *>
-                   ZIO.succeed {
-                     scheduledExecutorService.scheduleAtFixedRate(
-                       new Runnable {
-                         def run(): Unit =
-                           Unsafe.unsafe { implicit unsafe =>
-                             runtime.unsafe.run {
-                               ZIO.logInfo("Task started..") *>
-                                 clock.sleep(2.seconds) *>
-                                 clock.currentTime(TimeUnit.SECONDS).flatMap(now => ref.update(now :: _)) *>
-                                 ZIO.logInfo("Task completed")
-                             }.getOrThrowFiberFailure()
-                           }
-                       },
-                       3,
-                       5,
-                       TimeUnit.SECONDS
-                     )
-                   }
+                        ZIO.succeed {
+                          scheduledExecutorService.scheduleAtFixedRate(
+                            new Runnable {
+                              def run(): Unit =
+                                Unsafe.unsafe { implicit unsafe =>
+                                  runtime.unsafe.run {
+                                    ZIO.logInfo("Task started..") *>
+                                      clock.sleep(2.seconds) *>
+                                      clock.currentTime(TimeUnit.SECONDS).flatMap(now => ref.update(now :: _)) *>
+                                      ZIO.logInfo("Task completed")
+                                  }.getOrThrowFiberFailure()
+                                }
+                            },
+                            3,
+                            5,
+                            TimeUnit.SECONDS
+                          )
+                        }
             _      <- TestClock.adjust(7.seconds)
             _      <- ZIO.succeed(future.cancel(false))
             _      <- TestClock.adjust(11.seconds)
@@ -125,5 +125,5 @@ object TestClockSpecJVM extends ZIOBaseSpec {
           } yield assert(values.reverse)(equalTo(List(5L)))
         }
       )
-    ) @@ TestAspect.nonFlaky(10)
+    ) @@ TestAspect.nonFlaky(20)
 }

f818cd8ca015277f318837615bc67f3b355a9be9
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-08-14 19:01:51 +0530

    Update ClockPlatformSpecific.scala

--- core/native/src/main/scala/zio/ClockPlatformSpecific.scala
+++ core/native/src/main/scala/zio/ClockPlatformSpecific.scala
@@ -13,20 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package zio
+
+import java.util.concurrent.{ScheduledExecutorService, Executors, TimeUnit}
 import zio.stacktracer.TracingImplicits.disableAutoTrace
 import zio.{DurationSyntax => _}
 
-import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
-import scala.scalanative.loop._
 
 private[zio] trait ClockPlatformSpecific {
+  import ClockPlatformSpecific.Timer
   private[zio] val globalScheduler = new Scheduler {
     import Scheduler.CancelToken
 
     private[this] val ConstTrue  = () => false
     private[this] val ConstFalse = () => false
+
+    // Multi-threaded scheduler using ScheduledExecutorService
+    private val scheduler: ScheduledExecutorService = Executors.newScheduledThreadPool(2)
 
     override def schedule(task: Runnable, duration: Duration)(implicit unsafe: Unsafe): CancelToken =
       (duration: @unchecked) match {
@@ -35,116 +40,50 @@ private[zio] trait ClockPlatformSpecific {
           ConstTrue
         case zio.Duration.Infinity => ConstFalse
         case zio.Duration.Finite(nanos) =>
-          var completed = false
-
-          val handle = Timer.timeout(FiniteDuration(nanos, TimeUnit.NANOSECONDS)) { () =>
-            completed = true
-            task.run()
-          }
-          () => {
-            handle.clear()
-            !completed
-          }
+          val future = scheduler.schedule(task, nanos, TimeUnit.NANOSECONDS)
+          () => future.cancel(true)
       }
   }
 }
 
-// /**
-//  * Copy-pasted from scala.scalanative.loop
-//  *
-//  * This is temporary until we figure out why using it directly raises errors
-//  */
-// private object ClockPlatformSpecific {
-//   import scala.collection.mutable
-//   import scala.concurrent.Future
-//   import scala.scalanative.annotation.alwaysinline
-//   import scala.scalanative.libc.stdlib
-//   import scala.scalanative.loop.EventLoop
-//   import scala.scalanative.loop.LibUV._
-//   import scala.scalanative.loop.LibUVConstants._
-//   import scala.scalanative.runtime.Intrinsics._
-//   import scala.scalanative.runtime._
-//   import scala.scalanative.unsafe.Ptr
+private object ClockPlatformSpecific {
+  // Multi-threaded scheduler using ScheduledExecutorService
+  private val scheduler: ScheduledExecutorService = Executors.newScheduledThreadPool(2)
 
-//   private object HandleUtils {
-//     private val references = mutable.Map.empty[Object, Int]
+  final class Timer private (private val scheduledFuture: java.util.concurrent.ScheduledFuture[_]) extends AnyVal {
+    def clear(): Unit =
+      scheduledFuture.cancel(false)
+  }
 
-//     @alwaysinline def getData[T <: Object](handle: Ptr[Byte]): T = {
-//       // data is the first member of uv_loop_t
-//       val ptrOfPtr = handle.asInstanceOf[Ptr[Ptr[Byte]]]
-//       val dataPtr  = !ptrOfPtr
-//       if (dataPtr == null) null.asInstanceOf[T]
-//       else {
-//         val rawptr = toRawPtr(dataPtr)
-//         castRawPtrToObject(rawptr).asInstanceOf[T]
-//       }
-//     }
-//     @alwaysinline def setData(handle: Ptr[Byte], obj: Object): Unit = {
-//       // data is the first member of uv_loop_t
-//       val ptrOfPtr = handle.asInstanceOf[Ptr[Ptr[Byte]]]
-//       if (obj != null) {
-//         if (references.contains(obj)) references(obj) += 1
-//         else references(obj) = 1
-//         val rawptr = castObjectToRawPtr(obj)
-//         !ptrOfPtr = fromRawPtr[Byte](rawptr)
-//       } else {
-//         !ptrOfPtr = null
-//       }
-//     }
-//     private val onCloseCB: CloseCB = (handle: UVHandle) => {
-//       stdlib.free(handle)
-//     }
+  object Timer {
+    def delay(duration: FiniteDuration): UIO[Unit] =
+      for {
+        promise <- Promise.make[Nothing, Unit]
+        _       <- timeout(duration)(() => promise.succeed(()).unit)
+        _       <- promise.await
+      } yield ()
 
-//     @alwaysinline def close(handle: Ptr[Byte]): Unit =
-//       if (getData(handle) != null) {
-//         uv_close(handle, onCloseCB)
-//         val data    = getData[Object](handle)
-//         val current = references(data)
-//         if (current > 1) references(data) -= 1
-//         else references.remove(data)
-//         setData(handle, null)
-//       }
-//   }
+    def timeout(duration: FiniteDuration)(callback: () => Unit): Timer = {
+      val scheduledFuture = scheduler.schedule(
+        new Runnable {
+          override def run(): Unit = callback()
+        },
+        duration.toMillis,
+        TimeUnit.MILLISECONDS
+      )
+      new Timer(scheduledFuture)
+    }
 
-//   @alwaysinline final class Timer private (private val ptr: Ptr[Byte]) extends AnyVal {
-//     def clear(): Unit = {
-//       uv_timer_stop(ptr)
-//       HandleUtils.close(ptr)
-//     }
-//   }
-
-//   object Timer {
-//     import scala.scalanative.loop.LibUV._
-//     private val timeoutCB: TimerCB = (handle: TimerHandle) => {
-//       val callback = HandleUtils.getData[() => Unit](handle)
-//       callback.apply()
-//     }
-
-//     @alwaysinline private def startTimer(
-//       timeout: Long,
-//       repeat: Long,
-//       callback: () => Unit
-//     ): Timer = {
-//       val timerHandle = stdlib.malloc(uv_handle_size(UV_TIMER_T))
-//       uv_timer_init(EventLoop.loop, timerHandle)
-//       HandleUtils.setData(timerHandle, callback)
-//       val timer = new Timer(timerHandle)
-//       uv_timer_start(timerHandle, timeoutCB, timeout, repeat)
-//       timer
-//     }
-
-//     def delay(duration: FiniteDuration): Future[Unit] = {
-//       val promise = scala.concurrent.Promise[Unit]()
-//       timeout(duration)(() => promise.success(()))
-//       promise.future
-//     }
-
-//     def timeout(duration: FiniteDuration)(callback: () => Unit): Timer =
-//       startTimer(duration.toMillis, 0L, callback)
-
-//     def repeat(duration: FiniteDuration)(callback: () => Unit): Timer = {
-//       val millis = duration.toMillis
-//       startTimer(millis, millis, callback)
-//     }
-//   }
-// }
+    def repeat(duration: FiniteDuration)(callback: () => Unit): Timer = {
+      val scheduledFuture = scheduler.scheduleAtFixedRate(
+        new Runnable {
+          override def run(): Unit = callback()
+        },
+        duration.toMillis,
+        duration.toMillis,
+        TimeUnit.MILLISECONDS
+      )
+      new Timer(scheduledFuture)
+    }
+  }
+}

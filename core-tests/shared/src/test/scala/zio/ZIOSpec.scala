7514d7282c05ff75cc11ca0aaf87e39932601137
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-08-18 06:05:10 +0530

    Update ZIOSpec.scala

--- core-tests/shared/src/test/scala/zio/ZIOSpec.scala
+++ core-tests/shared/src/test/scala/zio/ZIOSpec.scala
@@ -1021,7 +1021,7 @@ object ZIOSpec extends ZIOBaseSpec {
       //   assertZIO(res)(equalTo(seq))
       // } @@ native(nonFlaky(20)),
       test("works on large lists") {
-        val n   = 1
+        val n   = 2
         val seq = List.range(0, 100000)
         val res = ZIO.foreachPar(seq)(ZIO.succeed(_)).withParallelism(n)
         assertZIO(res)(equalTo(seq))

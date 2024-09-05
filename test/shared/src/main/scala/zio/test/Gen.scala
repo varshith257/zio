55aaed02af3feeb3b72ffd61852c523d5c6914f3
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-09-06 04:23:53 +0530

    add implicit l

--- test/shared/src/main/scala/zio/test/Gen.scala
+++ test/shared/src/main/scala/zio/test/Gen.scala
@@ -861,7 +861,7 @@ object Gen extends GenZIO with FunctionVariants with TimeVariants {
    * not have any shrinking.
    */
   def uuid(implicit trace: Trace): Gen[Any, UUID] =
-    Gen.fromEffect(ZIO.randomWith(_.nextUUID))
+    Gen.fromZIO(ZIO.randomWith(_.nextUUID))
 
   /**
    * A sized generator of vectors.

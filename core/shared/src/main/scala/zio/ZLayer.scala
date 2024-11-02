d33d5cb1c513b5a5b6d36cba06e56778a8d38b3e
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-11-02 16:02:56 +0530

    fmt

--- core/shared/src/main/scala/zio/ZLayer.scala
+++ core/shared/src/main/scala/zio/ZLayer.scala
@@ -323,11 +323,11 @@ sealed abstract class ZLayer[-RIn, +E, +ROut] { self =>
    * Runs this layer by composing it with the specified layers for dependencies.
    * Returns an effect that produces `Unit`, assuming the layer outputs `Unit`.
    */
-  final def runWith[RIn1 <: RIn: Tag, ROut >: RIn: Tag](
-    layers: ZLayer[RIn1, Nothing, RIn]*
-  )(implicit trace: Trace): ZIO[RIn1 with Scope, E, Unit] = {
+  final def runWith[RIn0 <: RIn: Tag, RIn: Tag](
+    layers: ZLayer[RIn0, Nothing, RIn]*
+  )(implicit trace: Trace): ZIO[RIn0 with Scope, E, Unit] = {
     val composedLayer = layers.reduce(_ ++ _)
-    ZIO.scoped((composedLayer >>> self).build.unit)
+    ZIO.scoped((composedLayer >>> self).build).unit
   }
 
   /**

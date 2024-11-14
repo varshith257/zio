cabf14646cc51dd8dde94221bc8e2e2af811e826
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-11-15 01:15:44 +0530

    update raceFirst

--- core-tests/shared/src/test/scala/zio/ZIOSpec.scala
+++ core-tests/shared/src/test/scala/zio/ZIOSpec.scala
@@ -3138,9 +3138,7 @@ object ZIOSpec extends ZIOBaseSpec {
           acquire                 = backgroundBlockingStuff.fork
           release                 = ZIO.succeed(isRunning.set(false))
           eff                     = ZIO.acquireRelease(acquire)(_ => release)
-          _ <- ZIO.scoped {
-                 ZIO.raceFirst(eff, ZIO.never)
-               }
+          _                      <- ZIO.scoped(ZIO.succeed(ZIO.raceFirst(eff, Nil)))
         } yield assertCompletes
       } @@ timeout(10.seconds),
       test("mergeAll") {

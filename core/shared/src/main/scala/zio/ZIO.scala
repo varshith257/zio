c62f4521c9aea88fcfc9edc119639d581dedbb15
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-11-19 02:47:07 +0530

    reproduce

--- core/shared/src/main/scala/zio/ZIO.scala
+++ core/shared/src/main/scala/zio/ZIO.scala
@@ -1391,9 +1391,10 @@ sealed trait ZIO[-R, +E, +A]
     else
       ZIO.scoped[R1] {
         for {
-          Env <- ZEnvironment.empty[Scope]
+          scope <- ZIO.scope
+          Env <- ZEnvironment.empty.add(scope)
           leftFiber  <- self.provideSomeEnvironment[R1 with Scope](_.union[Scope](Env)).forkScoped
-          rightFiber <- that.provideSomeEnvironment[R1 with Scope](_.union[Scope](Envy)).forkScoped
+          rightFiber <- that.provideSomeEnvironment[R1 with Scope](_.union[Scope](Env)).forkScoped
           result <- (leftFiber.await race rightFiber.await).flatMap {
                       case Exit.Success(value) =>
                         ZIO.succeed(value) // Return the winning value
